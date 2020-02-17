7d2f3ce765b297d53a5452b81ca5be4f66fbda5f
raphaelschmitz <raphael.schmitz@me.com>
2020-02-17 22:49:59 +0100

    test
    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,31 @@ import { first } from 'rxjs/operators';
 import { User } from '@app/_models';
 import { UserService, AuthenticationService } from '@app/_services';
 
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
     loading = false;
     users: User[];
 
-    constructor(private userService: UserService) { }
+    name = 'Set iframe source';
+    url: string = "https://juliaraphael.de";
+    urlSafe: SafeResourceUrl;
+  
+    constructor(private userService: UserService, public sanitizer: DomSanitizer) { 
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://juliaraphael.de');
+
+        xhr.responseType = 'blob';
+        xhr.setRequestHeader('Authorization', 'Bearer ' );
+        xhr.send();
+        
+
+
+    }
+
 
     ngOnInit() {
         this.loading = true;
@@ -17,5 +36,9 @@ export class HomeComponent {
             this.loading = false;
             this.users = users;
         });
+
+
+        this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+
     }
 }
